0c29243cac7b4c4bf3192dd336b52165700dcfa5
Jakub Słota <jakub.slota@interia.pl>
2025-04-28 17:37:16 +0200

    definicja t_norm i t_konorm

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Agregacje")
+import numpy as np
+
+def t_norm_min(a, b):
+    return np.minimum(a, b)  # Minimum
+
+def t_norm_product(a, b):
+    return a * b  # Iloczyn
+
+def t_norm_lukasiewicz(a, b):
+    return np.maximum(0, a + b - 1)  # Łukasiewicz
+
+
+
+
+def t_conorm_max(a, b):
+    return np.maximum(a, b)  # Maksimum
+
+def t_conorm_probabilistic_sum(a, b):
+    return a + b - a * b  # Suma probabilistyczna
+
+def t_conorm_lukasiewicz(a, b):
+    return np.minimum(1, a + b)  # Łukasiewicz
